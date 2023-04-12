7136add67a2d63b8b608f1acb35df21d48d93151
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-13 00:45:23 +0600

    dfghfdgh

---
+++ index.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const cors = require('cors')
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from node crude!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`)
+// })
+
+
