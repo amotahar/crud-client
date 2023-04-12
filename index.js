bc8567e3c573a41dae27bda12cfa2f93876e4cda
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-13 01:01:21 +0600

    setup

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-// const express = require('express');
-// const cors = require('cors')
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello from node crude!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`listening on port ${port}`)
-// })
-
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello form CRUD')
+
+app.get('/', (req, res, next) => {
+  res.send('Hello CRUD!');
 })
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+  console.log('Listening on port ${port}');
 });
