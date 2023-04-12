e4ccd9d91fd90dc9a5851f9094f45d5701378202
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-13 00:50:22 +0600

    strong

--- index.js
+++ index.js
@@ -11,4 +11,15 @@
 //   console.log(`listening on port ${port}`)
 // })
 
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Hello form CRUD')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+});
